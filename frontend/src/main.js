769f674af8a20625f81a565ef59b0b3307b414d3
vlb103 <182360211+vlb103@users.noreply.github.com>
2025-06-02 13:31:16 +0300

    delete about, user

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,12 @@
 import "./style.css";
 import Router from "./router.js";
 import HomePage from "./pages/HomePage.js";
-import AboutPage from "./pages/AboutPage.js";
-import UserPage from "./pages/UserPage.js";
 import LoginPage from "./pages/LoginPage.js";
 import RegisterPage from "./pages/RegisterPage.js";
 import NotFoundPage from "./pages/NotFoundPage.js";
 
 new Router({
   "/": HomePage,
-  "/about": AboutPage,
-  "/user/:id": UserPage,
   "/login": LoginPage,
   "/register": RegisterPage,
 });
--- frontend/src/pages/UserPage.js
+++
@@ -1,6 +0,0 @@
-import Page from "./Page.js";
-export default class UserPage extends Page {
-  render() {
-    return `<h1>User Profile</h1><p>Hello, ${this.params.id}</p>`;
-  }
-}

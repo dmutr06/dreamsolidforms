b8a778e3abe2c211f320eb1d5a031e7769ddf377
voloshyyn <25062006vas@gmail.com>
2025-06-05 01:58:14 +0300

    Write forms controller

--- backend/src/forms/forms.controller.ts
+++ backend/src/forms/forms.controller.ts
@@ -1,45 +1,48 @@
 import { inject, injectable } from "inversify";
-import { Request, Response } from "express";
+import { Response } from "express";
 import { Controller } from "../lib/controller";
 import { get, post, use } from "../lib/decorators";
 import { TYPES } from "../inversify.types";
 import { IFormsService } from "./forms.service.interface";
-import { AuthMiddleware } from "../common/auth.middleware";
-import { FormsRepository } from "./forms.repository";
-import { ValidateMiddlleware } from "../common/validate.middleware";
 import { CreateFormDto } from "./dtos/createForm.dto";
-import { AuthedRequest, TypedRequest } from "../common/customRequest";
+import { ValidateMiddlleware } from "../common/validate.middleware";
+import { AuthedRequest } from "../common/customRequest";
 import { HttpError } from "../common/httpError";
 import { AuthGuard } from "../common/auth.guard";
 
-// TODO write users service and rewrite handler
-
 @injectable()
 export class FormsController extends Controller {
-    constructor(@inject(TYPES.FormsService) private formsService: IFormsService, @inject(TYPES.FormsRepository) private formsRepo: FormsRepository) {
+    constructor(
+        @inject(TYPES.FormsService) private formsService: IFormsService
+    ) {
         super();
     }
-    
+
     @post("/")
-    @use(new ValidateMiddlleware(CreateFormDto))
     @use(new AuthGuard())
+    @use(new ValidateMiddlleware(CreateFormDto))
     async createForm(req: AuthedRequest<CreateFormDto>, res: Response) {
-        const result = await this.formsRepo.createForm({ ...req.body, userId: req.user });
+        const form = await this.formsService.createForm({
+            ...req.body,
+            userId: req.user,
+        });
 
-        if (!result)
-            throw new HttpError(400, "Bad request");
-
-        res.json({ statusCode: 201, message: "Created" });
+        res.status(201).json({ message: "Form created", form });
     }
 
     @get("/:id")
-    // @use(new AuthMiddleware())
-    async getForm(req: Request, res: Response) {
-        return res.json(await this.formsRepo.getFormById(req.params.id));
-    }
+    @use(new AuthGuard())
+    async getFormById(req: AuthedRequest, res: Response) {
+    const { id } = req.params as { id: string };
+
+    const form = await this.formsService.getFormById(id);
+    res.json(form);
+}
 
     @get("/")
-    async getAllForms(req: Request, res: Response) {
-        return res.json(await this.formsRepo.getAllForms());
+    @use(new AuthGuard())
+    async getUserForms(req: AuthedRequest, res: Response) {
+        const forms = await this.formsService.getFormById(req.user);
+        res.json(forms);
     }
 }

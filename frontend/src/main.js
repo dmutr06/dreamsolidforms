8dd9e660c258e96690c36323c66a6695e616c9e3
dmutro <shaduraa.dmutroo@gmail.com>
2025-06-04 15:12:40 +0300

    Revert "Merge pull request #1 from dmutr06/feature/form-pages"
    This reverts commit 7c417655f3ad88fbd77265be03701db5a9438c54, reversing
    changes made to 95364b1f9d981c266beacef89e00ef0496124b5b.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,18 +5,10 @@ import Router from "./router.js";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import FormPassPage from "./pages/FormPassPage";
-import ResultsPage from "./pages/ResultsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-
-
 
 new Router({
   "/": HomePage,
   "/login": LoginPage,
   "/register": RegisterPage,
-  "/forms/:id/pass": FormPassPage,
-  "/forms/:id/results": ResultsPage,
 });
-
-const router = new Router(routes);
